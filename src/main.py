33adcc2a6668303a5ca4ed2e93a27273f446ec3f
Thang Vu <ducthang310@gmail.com>
2024-08-03 14:19:05 +0700

    Debug

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
 from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
 from image_processing import generate_video_from_images
 from video_processing import extract_subvideo
